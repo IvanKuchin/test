d0be81b653d10372cb58f542d9c8b87c2c0569d8
IvanKuchin <ivan.kuchin@gmail.com>
2021-04-16 21:40:25 -0400

    Create run.sh

---
+++ supplemental/docker/qa/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mysqladmin -u root -ptoor -h mysql ping
+
+mysql -u root -ptoor -h mysql -e "SHOW DATABASES;"
+mysql -u root -ptoor -h mysql -e "CREATE DATABASE db;"
+mysql -u root -ptoor -h mysql db < /root/dump.sql"
+mysql -u root -ptoor -h mysql -e "show tables;"
+
