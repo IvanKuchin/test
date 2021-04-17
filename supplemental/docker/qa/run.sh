d24e0f6109ca679490ccee7575144185ba68fbfa
IvanKuchin <ivan.kuchin@gmail.com>
2021-04-16 22:03:24 -0400

    Update run.sh

--- supplemental/docker/qa/run.sh
+++ supplemental/docker/qa/run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+pwd
 mysqladmin -u root -ptoor -h mysql ping
 
 mysql -u root -ptoor -h mysql -e "SHOW DATABASES;"
-mysql -u root -ptoor -h mysql db < /root/dump.sql
+mysql -u root -ptoor -h mysql db < ./supplemental/docker/qa/dump.sql
 mysql -u root -ptoor -h mysql db -e "show tables;"
 
 cd ./build && ctest --output-on-failure
